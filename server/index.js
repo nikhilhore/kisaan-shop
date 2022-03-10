ac4c11110987148e959df194efa76ba7174c6e32
nikhilhore <niknsh5@gmail.com>
2022-03-10 22:59:07 +0530

    modified package.json

--- server/index.js
+++ server/index.js
@@ -15,6 +15,10 @@ app.get('/api', (req, res) => {
     res.json({ message: "Hello from server" });
 });
 
+app.get('/hello', (req, res)=>{
+    res.json({msg: 'Hii'});
+});
+
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
